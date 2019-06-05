268b07142cc50c6c77d1f9324576a54137ba5772
krosss1 <47490643+krosss1@users.noreply.github.com>
2019-06-05 16:11:54 -0600

    Add files via upload

---
+++ Week_13_Javascript_Homework/index.js
@@ -0,0 +1,40 @@
+var tableData = data;
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+
+    Object.entries(ufoReport).forEach(function([key, value]){
+        console.log(key, value);
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
+
+var submit = d3.select("#filter-btn");
+var empty = d3.select("tbody")
+submit.on("click", function() {
+    empty.html("")
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    var filteredData = tableData.filter(bydate => bydate.datetime === inputValue);
+
+    console.log(filteredData);
+
+    filteredData.forEach(function(filteredReport) {
+        console.log(filteredReport);
+        var row = tbody.append("tr");
+    
+        Object.entries(filteredReport).forEach(function([key, value]){
+            console.log(key, value);
+            var cell = tbody.append("td");
+            cell.text(value);
+         });
+       });
+});
